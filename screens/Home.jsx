14a16ce9da2acc4e8df6ad54f73b819836f0e5aa
MousGamee <moussa.sagna@hotmail.fr>
2020-10-21 12:25:24 +0200

    debut de la page profil

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -3,8 +3,8 @@ import { View, Text } from 'react-native'
 
 export default function Home() {
     return (
-        <View>
-            <Text>Home page</Text>
+        <View style={{flex : 1, justifyContent : 'center', alignItems : 'center'}}>
+            <Text>Liste des articles enregistrés</Text>
         </View>
     )
 }
--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -16,11 +16,14 @@ export default function Profile() {
                 <View style={{width : 200, height : 200, borderRadius : 100, overflow : 'hidden'}}>
                     <Image
                     style={{flex : 1, width : undefined, height : undefined}}
-                        source={require('../assets/profile.jpg')}
+                        source={require('../assets/image3.png')}
                         resizeMode="center"
                         />
-
+                        
                 </View>
+                <Text style={{fontSize : 24, fontWeight : '700', justifyContent : "center", alignSelf : "center"}}>
+                    Profil
+                 </Text>
             </View>
         </SafeAreaView>
     )
